c4879463669083890b6cf8be4feca178741e468e
ehr174 <ehr174@gmail.com>
2012-12-03 21:15:28 +0900

    changed modal class.

--- example/modal.dart
+++ example/modal.dart
@@ -3,9 +3,41 @@ import 'package:dartstrap/dartstrap.dart';
 
 void main() {
 
-  document.query('#ShowModal').on.click.add((_){
-    Modal modal = new Modal('#myModal');
+  document.query('#notSetProperties').on.click.add((_){
+    Modal modal = new Modal();
     modal.show();
   });
+
+
+  document.query('#info').on.click.add((_){
+    Modal modal = new Modal();
+    modal.header = '<h3 class="text-info">INFO</h3>';
+    modal.body   = '<p>info info info info info<br>info info info<br>info info info info info info info info</p>';
+    modal.footer = '<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button> <button id="vote" class="btn btn-primary">VOTE</button>';
+    modal.show();
+
+    document.query('#vote').on.click.add((_){
+      print('clicked vote button.');
+    });
+  });
+
+  document.query('#success').on.click.add((_){
+    Modal modal = new Modal();
+    modal.header = '<h3 class="text-success">SUCCESS</h3>';
+    modal.show();
+  });
+
+  document.query('#warning').on.click.add((_){
+    Modal modal = new Modal();
+    modal.header = '<h3 class="text-warning">WARNING</h3>';
+    modal.show();
+  });
+
+  document.query('#error').on.click.add((_){
+    Modal modal = new Modal();
+    modal.header = '<h3 class="text-error">ERROR</h3>';
+    modal.show();
+  });
+
 
 }
--- packages/dartstrap/src/modal.dart
+++ packages/dartstrap/src/modal.dart
@@ -1,39 +1,88 @@
 part of dartstrap;
 
 class Modal {
-  String _targetElementID;
+  String topDivId;
+  String topDivClass;
+  String header;
+  String body;
+  String footer;
 
-  Modal(this._targetElementID);
-
-  String get targetElementID => this._targetElementID;
-  void set targetElementID( String targetElementID ) {
-    this._targetElementID = targetElementID;
-  }
-
-
-  /**
-   * toggle
-   *
-   * */
-  void toggle() {
-    Util.execJS( "jQuery('${this._targetElementID}').modal('toggle');" );
+  Modal() {
+    topDivId    = '__myModal';          //default div id
+    topDivClass = 'modal hide fade';    //default class options
+    header      = '<h3 class="">This is default header Property.</h3>';
+    body        = '<p>This is default body Property</p>';
+    footer      = '<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>';
   }
 
   /**
    * show
    *
    * */
-  void show() {
-//    Util.execJS( "\$('${this._targetElementID}').modal('show');", false );
-    Util.execJS( "jQuery('${this._targetElementID}').modal('show');" );
+  DivElement show() {
+    Element parent = document.query('body');
+    if( parent == null ) {
+      throw( new Exception('Target Not Found. [<body>]') );
+    }
+
+    DivElement el = document.query('#${this.topDivId}');
+    if( el != null ){ el.remove(); }
+
+    String div = _buildModalDiv();
+    DivElement divElement = new Element.html(div);
+
+    parent.nodes.add(divElement);
+
+    Util.execJS( "jQuery('#${this.topDivId}').modal('show');" );
+
+    return divElement;
   }
 
-  /**
-   * hide
-   *
-   * */
-  void hide() {
-    Util.execJS( "jQuery('${this._targetElementID}').modal('hide');" );
+
+
+/*
+  <div id="myModal" class="modal hide fade">
+    <div class="modal-header">
+      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+      <h3 class="text-info">Hello Modal</h3>
+    </div>
+    <div class="modal-body">
+      <p>This is body. body. body. body.</p>
+    </div>
+    <div class="modal-footer">
+      <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+    </div>
+  </div>
+*/
+  String _buildModalDiv() {
+    StringBuffer div = new StringBuffer();
+    div.add('<div id="${topDivId}" class="${topDivClass}">');
+    div.add('  <div class="modal-header">');
+    div.add('    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>');
+    div.add('    ${header}');
+    div.add('  </div>');
+    div.add('  <div class="modal-body">');
+    div.add('    ${body}');
+    div.add('  </div>');
+    div.add('  <div class="modal-footer">');
+    div.add('    ${footer}');
+    div.add('  </div>');
+    div.add('</div>');
+
+    return div.toString();
   }
+
+//  void toggle() {
+//    Util.execJS( "jQuery('${this._targetElementID}').modal('toggle');" );
+//  }
+//
+//  void show() {
+////    Util.execJS( "\$('${this._targetElementID}').modal('show');", false );
+//    Util.execJS( "jQuery('${this._targetElementID}').modal('show');" );
+//  }
+//
+//  void hide() {
+//    Util.execJS( "jQuery('${this._targetElementID}').modal('hide');" );
+//  }
 
 }
